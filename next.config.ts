c0066fa31a199b31b990e661eb82a5a56ed40a45
Irwanto Siregar <irwanto.regar30@gmail.com>
2025-04-21 13:33:01 +0700

    feat: crud rent car

--- next.config.ts
+++ next.config.ts
@@ -3,15 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
-  images: {
-    domains: ["cdn.dummyjson.com"],
-    remotePatterns: [
-      {
-        hostname: "**",
-        protocol: "https",
-      },
-    ],
-  },
   async redirects() {
     return [
       {
